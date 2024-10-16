7a890065dc86b61e92891bb15fece4eee6680bf7
k41531 <me@kaisei.dev>
2024-10-16 23:33:25 +0900

    enable flakes

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,9 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Enabling Flakes Support
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
