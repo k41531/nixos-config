1405ade723e5d563dc735e2e31eeee28dcb65d9e
k41531 <me@kaisei.dev>
2024-10-18 23:05:00 +0900

    add tmux

--- home.nix
+++ home.nix
@@ -4,8 +4,17 @@
   home.username = "k41531";
   home.homeDirectory = "/home/k41531";
   
-  programs.neovim = {
-    enable = true;
+  programs = {
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+
+    tmux = {
+      enable = true;
+      prefix = "C-a";
+    };
   };
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
