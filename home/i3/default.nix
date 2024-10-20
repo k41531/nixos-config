19b674db95226cb5bc79cd9079f8193a480a2659
k41531 <me@kaisei.dev>
2024-10-20 22:56:44 +0900

    update i3bar style

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -19,6 +19,24 @@
         inner = 10;
         outer = 5;
       };
+
+      bars = [
+        {
+          colors = {
+            background = "#282A36";
+	    focusedWorkspace = {
+              background = "#1D1F28";
+	      border = "#1D1F28";
+	      text = "#FDFDFD";
+	    };
+	    inactiveWorkspace = {
+              background = "#282A36";
+	      border = "#282A36";
+	      text = "#BEBEC1";
+	    };
+          };
+        }
+      ];
     };
   };
 }
